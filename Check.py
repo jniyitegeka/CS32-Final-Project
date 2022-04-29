ffdc823d3f4f6f2ffa70b75bc92581b482ece755
jniyitegeka <102566924+jniyitegeka@users.noreply.github.com>
2022-04-29 11:25:20 -0400

    Create Check.py

---
+++ Check.py
@@ -0,0 +1,8 @@
+"""CS32 helper library for Act I, Scene II"""
+
+
+def check_arg(arg: object, ttype: type, arg_name: str):
+    if not isinstance(arg, ttype):
+        raise TypeError(
+            f"The argument {arg_name} is of type {type(arg)}, but this function requires {ttype}"
+        )
